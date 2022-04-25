a36100793328c85969e5cf5188b73241738b4654
Jano <Jano@DESKTOP-6LEGV70>
2022-04-25 19:15:06 -0300

    - CrossOrigin *

--- src/main/java/com/springboot/app/business/passenger/PassengerController.java
+++ src/main/java/com/springboot/app/business/passenger/PassengerController.java
@@ -2,6 +2,7 @@ package com.springboot.app.business.passenger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.app.business.passenger.model.PassengerTO;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("passenger")
 public class PassengerController {
