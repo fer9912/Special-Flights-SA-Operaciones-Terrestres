ff5cbcc8b7f7ecd35ae1563c2a6cf6aa9bb6ba90
Jano <Jano@DESKTOP-6LEGV70>
2022-04-26 13:24:03 -0300

    - metodo ejemplo de lista de PassengerDEs en repositorio

--- src/main/java/com/springboot/app/repositories/PassengerRepository.java
+++ src/main/java/com/springboot/app/repositories/PassengerRepository.java
@@ -1,5 +1,6 @@
 package com.springboot.app.repositories;
 
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -12,4 +13,5 @@ public interface PassengerRepository extends JpaRepository<PassengerDE, Long>, J
 
 	PassengerDE findByIdPassenger(int idPassenger);
 
+	List<PassengerDE> findByName(String name);
 }
--- src/main/java/com/springboot/app/utils/Util.java
+++ src/main/java/com/springboot/app/utils/Util.java
@@ -2,4 +2,8 @@ package com.springboot.app.utils;
 
 public class Util {
 
+	public static boolean isEmptyOrNull(String any) {
+		return any == null || any.trim().equals("");
+	}
+
 }
